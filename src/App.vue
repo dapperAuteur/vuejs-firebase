1019c690c361369b2ee411c7eb7b5674594cf941
awe <aweful@awews.com>
2017-04-25 20:09:58 -0700

    complete firebase bookshelf app, need to host and deploy

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,121 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <div id="app" class="container">
+    <div class="page-header">
+      <h1>Vue.js 2 & Firebase Sample App</h1>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3>Add Book</h3>
+      </div>
+      <div class="panel-body">
+        <form id="form" class="form-inline" v-on:submit.prevent="addBook">
+          <div class="form-group">
+            <label for="bookTitle">Title:</label>
+            <input type="text" id="bookTitle" class="form-control" v-model="newBook.title">
+          </div>
+          <div class="form-group">
+            <label for="bookAuthor">Author:</label>
+            <input type="text" id="bookAuthor" class="form-control" v-model="newBook.author">
+          </div>
+          <div class="form-group">
+            <label for="bookUrl">URL:</label>
+            <input type="text" id="bookUrl" class="form-control" v-model="newBook.url">
+          </div>
+          <input type="submit" class="btn btn-primary" value="Add Book" />
+        </form>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3>Books Lists</h3>
+      </div>
+      <div class="panel-body">
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th>
+                Title
+              </th>
+              <th>
+                Author
+              </th>
+              <th>
+                Delete
+              </th>
+              <th>
+                Edit
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="book in books">
+              <td>
+                <a v-bind:href="book.url">{{ book.title }}</a>
+              </td>
+              <td>
+                {{ book.author }}
+              </td>
+              <td>
+                <span class="glyphicon glyphicon-trash" v-on:click="removeBook(book)"></span>
+              </td>
+              <td>
+                <span class="glyphicon glyphicon-pencil" v-on:click="editBook(book)"></span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+  import Firebase from 'firebase'
+
+  let config = {
+    apiKey: "AIzaSyBzqu6pE0S5ME21uf8CxuHKWMI9BhNBqs8",
+    authDomain: "fb-quickstart-js.firebaseapp.com",
+    databaseURL: "https://fb-quickstart-js.firebaseio.com",
+    // projectId: "fb-quickstart-js",
+    storageBucket: "fb-quickstart-js.appspot.com",
+    messagingSenderId: "752446434071"
+  };
+
+  let app = Firebase.initializeApp(config)
+  let db = app.database()
+  let booksRef = db.ref('books')
+
 export default {
-  name: 'app'
+  name: 'app',
+  firebase: {
+    books: booksRef
+  },
+  data() {
+    return {
+      newBook: {
+        title: '',
+        author: '',
+        url: ''
+      }
+    }
+  },
+  methods: {
+    addBook: function(){
+      booksRef.push(this.newBook)
+      this.newBook.title = '';
+      this.newBook.author = '';
+      this.newBook.url = '';
+    },
+    removeBook: function(book){
+      booksRef.child(book['.key']).remove();
+    },
+    editBook: function(book){
+      console.log('editBook ');
+      console.log(book);
+    }
+  }
 }
 </script>
 
@@ -16,7 +124,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /*text-align: center;*/
   color: #2c3e50;
   margin-top: 60px;
 }
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,22 +1,43 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <!-- <div class="panel-body">
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Author</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="book in books">
+          <td><a v-bind:href="book.url">{{book.title}}</a></td>
+          <td>{{book.author}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title">Add New Books</h3>
+      </div>
+      <div class="panel-body">
+         <form id="form" class="form-inline" v-on:submit.prevent="addBook">
+          <div class="form-group">
+            <label for="bookTitle">Title:</label>
+            <input type="text" id="bookTitle" class="form-control" v-model="newBook.title">
+          </div>
+          <div class="form-group">
+            <label for="bookAuthor">Author:</label>
+            <input type="text" id="bookAuthor" class="form-control" v-model="newBook.author">
+          </div>
+          <div class="form-group">
+            <label for="bookUrl">Url:</label>
+            <input type="text" id="bookUrl" class="form-control" v-model="newBook.url">
+          </div>
+          <input type="submit" class="btn btn-primary" value="Add Book">
+        </form>
+      </div>
+    </div> -->
   </div>
 </template>
 
@@ -25,7 +46,11 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      newBook: {
+          title: '',
+          author: '',
+          url: 'http://'
+      }
     }
   }
 }
